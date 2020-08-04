5a694cb6cc3a7cba2065693f51f2a02afce6c169
Alexis Rosado <ARosado@medicalcardsystem.com>
2020-08-04 15:21:08 -0400

    progresso

---
+++ Modelo/Services/ProjectoPruebaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Prueba_Project.Modelo;
+using System.Linq;
+using System.Web;
+
+namespace Prueba_Project.Modelo.Services
+{
+    public class ProjectoPruebaDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
